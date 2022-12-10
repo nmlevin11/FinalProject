1390c5579f0a1cdbfef09d16b59eea4bfada59cb
nmlevin11 <nmhinde@gmail.com>
2022-12-10 09:42:57 -0600

    Added in a bunch of text and prediction section

--- server.R
+++ server.R
@@ -3,7 +3,7 @@ library(shiny)
 library(caret)
 library(tidyverse)
 library(DT)
-#data("GermanCredit")
+
 
 #Import the data
 red_data <- read_delim("winequality-red.csv", delim = ";")
@@ -19,11 +19,16 @@ wine_data <- wine_data %>% rename(fixed_acidity = 'fixed acidity', volatile_acid
 
 #Add variable for quality with >5 = good and otherwise bad
 wine_data <- wine_data %>% mutate(quality = if_else(qual_orig >5, "good", "bad") )
-wine_data$quality <- factor(wine_data$quality)
 wine_data <- wine_data[,-12]
-#wine_data <- wine_data %>% select(alcohol, volatile_acidity, type, sulphates, residual_sugar, total_SO2, quality)
+wine_data$quality <- factor(wine_data$quality)
+wine_data <- wine_data %>% select(alcohol, volatile_acidity, type, sulphates, residual_sugar, total_SO2, quality)
+
+#Run logistic regression with all variables to use with prediction tab.
+pred_model <- glm(quality ~ ., data = wine_data, family = "binomial")
 
 shinyServer(function(input, output, session){
+  #Data Tab Header Text
+  output$header <- renderText(paste0("Data summaries for ", input$x_choice))
   #Table Setup
   output$tab <- renderDataTable({
     variable_choice <- input$x_choice
@@ -41,23 +46,87 @@ shinyServer(function(input, output, session){
   })
   #Plot setup. If statements used to decide which plot to make. For now just put a plot in.
   output$dataPlot <- renderPlot({
+    #Filter Data
+    if(input$filter_choice == "Red Only"){plot_data <- wine_data %>% filter(type == "red")
+    }else if(input$filter_choice == "White Only"){plot_data <- wine_data %>% filter(type == "white")
+    } else {plot_data <- wine_data}
+    
+    #Create plot chosen
     if(input$plot_choice=="Histogram"){
-      ggplot(data=wine_data, aes(x=get(input$x_choice))) + 
-        geom_histogram()
+      ggplot(data=plot_data, aes(x=get(input$x_choice))) + 
+        geom_histogram(aes(fill=quality)) +
+        labs(x = input$x_choice)
     } else {
-      ggplot(data=wine_data, aes(x=get(input$x_choice))) + 
-        geom_boxplot()
+      ggplot(data=plot_data, aes(x=get(input$x_choice), y=quality)) + 
+        geom_boxplot()+
+        labs(x = input$x_choice)
     }
   })
   
   #Table of data for data tab
   data_selected <- reactive({
-    wine_data %>%
-      select(input$data_choice)
+    if(input$row_choice == "Include All"){wine_data %>% select(input$data_choice)
+    } else if(input$row_choice == "Red Only"){wine_data %>% select(input$data_choice) %>% filter(type == "red")
+      }else {wine_data %>% filter(type == "white") %>% select(input$data_choice) 
+      }
   })
   output$data_table <- renderDataTable({
     data_selected()
   })
+  
+  #Modeling section. Make this work with a run button
+  #model_string <- eventReactive(input$go, {paste(input$pred_choice, collapse = "+")
+  #})
+  
+  set.seed(371)
+  
+  #Allow this to be user-defined
+  #prop_choice <- 0.7
+  #m_choice <- 3
+  
+  #eventReactive(input$go, {
+    train_index <- createDataPartition(wine_data$quality, p=0.7, list=FALSE)
+    data_train <- wine_data[train_index, ]
+    data_test <- wine_data[-train_index, ]
+  #})
+    
+    #regression_fit <- eventReactive(input$go, {train(quality ~ alcohol, 
+                                                  #  data = data_train, method= "glm", 
+                                                   # family = "binomial", 
+                                                  #  preProcess = c("center", "scale"), 
+                                                   # trControl = trainControl(method = "cv", 
+                                                    #                         number = 10))
+   # })
+    #output$reg_summary <- renderTable({summary(regression_fit$finalModel)})
+    output$reg_results <- renderDataTable({
+      if(input$go){
+        model_string <- paste(input$pred_choice, collapse = "+")
+        regression_fit <- train(quality ~ model_string, data = data_train, method= "glm", 
+                                family = "binomial", 
+                                preProcess = c("center", "scale"), 
+                                trControl = trainControl(method = "cv", 
+                                                         number = 10))
+        data.frame(regression_fit$results)}
+    })
+    
+    
+  #Prediction. Need to figure out getting an output here.
+  output$pred_text <- renderText({
+    prediction_values <- data.frame(alcohol = input$num_alcohol, 
+                                    volatile_acidity = input$num_acid, 
+                                    type = input$num_type, 
+                                    sulphates = input$num_sulphate,
+                                    residual_sugar = input$num_sugar, 
+                                    total_SO2 = input$num_SO2)
+    predict_qual <- predict(pred_model, newdata = prediction_values, type = "response", se.fit=TRUE)
+    predict_qual_round <- round(predict_qual[[1]], 3)
+    predict_se_round <- round(predict_qual[[2]], 3)
+    result <- (paste0("The predicted probability of good quality is ", predict_qual_round," with a
+                  standard error of ", predict_se_round))
+    result
+  })
+
+  
     
   
   # downloadHandler() takes two arguments, both functions.
--- ui.R
+++ ui.R
@@ -3,6 +3,7 @@ library(caret)
 library(shiny)
 library(DT)
 library(shinydashboard)
+library(png)
 
 sidebar <- dashboardSidebar(
   sidebarMenu(
@@ -16,47 +17,103 @@ sidebar <- dashboardSidebar(
 body <- dashboardBody(
   tabItems(
     tabItem(tabName = "About",
-            mainPanel(h2("About this App"), plotOutput("plot2")
+            sidebarLayout(sidebarPanel(img(src = "wine glass.png", height = "100%", 
+                                           width = "100%", align = "center")),
+              mainPanel(h2("About this App"),
+                      h4("Have you ever sat down at the end of the day with a nice 
+                         glass of wine and wondered if you could make the experience
+                         nerdier? If the answer is yes, this may be just the app for 
+                         you. The purpose of this app is to model wine quality based
+                         on physiochemical tests. The app allows the user to perform
+                         exploratory data analysis, compare three different models,
+                         and make predictions for the quality result of future data
+                         points based on user-inputted values."),
+                      h4("The data used in this app comes from two datasets, one for
+                         red wine and one for white, from the UCI Machine Learning
+                         Repository. The full datasets includes 11 physiochemical values,
+                         like sulphates and percent alcohol, and a sensory quality
+                         score for each wine. For the purposes of this analysis, quality 
+                         has been dichotomized such that a score greater than 5 is 
+                         considered 'good' and a score of 5 or below is considered 'bad.'"),
+                      h4("The full dataset can be accessed", a(href = 
+                      "https://archive.ics.uci.edu/ml/datasets/Wine+Quality", "here")),
+                      h4("The app contains 4 tabs:"), 
+                      h4("1. this About tab"),
+                      h4("2. a Data Exploration tab with user configurable plots and
+                         summary statistics"),
+                      h4("3. A Modeling tab to compare 3 different predictive models,
+                         logistic regression, classification tree, and random forest."),
+                      h4("4. a Data tab that allows the user to subset the dataset and
+                         download it.")
+                      )
             )
-            
     ),
     
-    tabItem(tabName = "EDA",
+    tabItem(tabName = "EDA", 
             sidebarLayout(
               sidebarPanel(h1("Data Exploration"),
-                           selectInput("filter_choice", "Filter by Wine Type",
-                                       choices = c("Include All", 
-                                                   "Red Only",
-                                                   "White Only")),
-                           selectInput("plot_choice", "Select Plot Type", 
-                                       choices = c("Histogram", "Boxplot")),
-                           selectInput("x_choice", "Select Plot Variable",
+                           selectInput("summary_choice", "Choose summary statistic for table",
+                                       choices = c("mean", "median")),
+                           selectInput("x_choice", "Select variable to summarize",
                                        choices = c("alcohol", 
                                                    "volatile_acidity", 
                                                    "sulphates", 
                                                    "residual_sugar",
-                                                   "total_SO2"),),
-                           selectInput("summary_choice", "Choose summary statistic",
-                                       choices = c("mean", "median"))
+                                                   "total_SO2")),
+                           selectInput("plot_choice", "Select Plot Type", 
+                                       choices = c("Histogram", "Boxplot")),
+                           selectInput("filter_choice", "Filter Plot by Wine Type",
+                                       choices = c("Include All", 
+                                                   "Red Only",
+                                                   "White Only"))
               ),
-              mainPanel(h1("Text"), 
+              mainPanel(h1(textOutput("header")),
+                #h1("Data Summaries"), 
                         dataTableOutput("tab"),
                         plotOutput("dataPlot")
               )
             )
     ),
-    tabItem(tabName = "Model",
-            #h2("Modeling")
+    tabItem(tabName = "Model", withMathJax(),
+            
             mainPanel(
               title = "Modeling",
               tabsetPanel(type = "tabs",
                           tabPanel("Model Info", 
-                                   h2("Logistic Regression"),
-                                   h4("Insert info about regression model"),
-                                   h2("Classification Tree"),
-                                   h4("insert info about model"),
-                                   h2("Random Forest"),
-                                   h4("Insert info about model")
+                                   h3("Logistic Regression"),
+                                   h5("In the logistic regression model the goal
+                                      is to model a binary outcome using a linear 
+                                      equation where the logit function is used as a 
+                                      link between the mean and the linear form of
+                                      the model. The logit function is the following:"),
+                                   helpText('$$ln(\\frac{p}{1-p})$$'),
+                                   h5("One benefit of using a regression model is that
+                                      it is typically the simplest model. The downside 
+                                      is that it is typically not the best-performing
+                                      for prediction."),
+                                   h3("Classification Tree"),
+                                   h5("In a tree based model, the predictor space is
+                                      split into regions with different predictions
+                                      for each region. For a classification tree,
+                                      usually the most prevalent class in the region
+                                      is used as the prediction. Often for 
+                                      classification trees, the splits are selected 
+                                      by minimizing the Gini index:"),
+                                   helpText('$$2p(1-p)$$'),
+                                   h5("One benefit of trees is that they are easy to
+                                      understand and interpret output. One con is that
+                                      small changes in data can vastly change the tree."),
+                                   h3("Random Forest"),
+                                   h5("A random forest model is an extension of a
+                                      tree based model where multiple trees are created
+                                      from bootstrap samples and the results are 
+                                      averaged. Each individual tree uses a subset (m) 
+                                      of the predictiors (p).for classification trees,
+                                      usually m is \\(\\sqrt{p}\\)"),
+                                   h5("One pro of using a random forest is that it 
+                                      will often perform better for prediction over the
+                                      single tree and regression models. One con is that 
+                                      you lose interprettability.")
                           ),
                           tabPanel("Modeling", 
                                    sidebarPanel(
@@ -76,18 +133,55 @@ body <- dashboardBody(
                                      sliderInput("cp_choice", label = "CP for Classification Tree fit",
                                                  min=0, max = 0.1, step = 0.005, value = 0.05),
                                      sliderInput("m_choice", label = "m for Random Forest fit",
-                                                 min = 1, max = 5, step = 1, value = 3)
-                                   )),
-                          tabPanel("Prediction", 
-                                   sidebarPanel(h3("Select model for prediction"),
-                                                selectInput("model_choice", 
-                                                            label = "",
-                                                            choices = 
-                                                              c("Regression", 
-                                                                "Tree", 
-                                                                "Random Forest"))
-                                                #Need to figure out selecting values of predictors.
-                                   )
+                                                 min = 1, max = 5, step = 1, value = 3),
+                                     actionButton("go", "Run Models")
+                                   ),
+                                   mainPanel(dataTableOutput("reg_results"))
+                                   ),
+                          tabPanel("Prediction", sidebarLayout(
+                                   sidebarPanel(h4("Select values of predictors"),
+                                                numericInput("num_alcohol",
+                                                             "Percent alcohol",
+                                                             value = 0,
+                                                             min = 0,
+                                                             max = 20,
+                                                             step = 1),
+                                                numericInput("num_acid",
+                                                             "Volatile Acidity",
+                                                             value = 0,
+                                                             min = 0,
+                                                             max = 2,
+                                                             step = 0.05),
+                                                numericInput("num_sulphate",
+                                                             "Sulphates",
+                                                             value = 0,
+                                                             min = 0,
+                                                             max = 2,
+                                                             step = 0.05),
+                                                numericInput("num_sugar",
+                                                             "Residual Sugar",
+                                                             value = 0,
+                                                             min = 0,
+                                                             max = 10,
+                                                             step = 0.1),
+                                                numericInput("num_SO2",
+                                                             "Total SO2",
+                                                             value = 0,
+                                                             min = 0,
+                                                             max = 200,
+                                                             step = 5),
+                                                selectInput("num_type", 
+                                                            "Type",
+                                                            choices = c("red", 
+                                                                        "white"))
+                                   ),
+                                   mainPanel(h2("Prediction"),
+                                             h5("Based on the selections made on the left,
+                                                our prediction using a logistic regression
+                                                model with all variables included is the 
+                                                following:"),
+                                     textOutput("pred_text"))
+                          )
                           )
               )
             )
@@ -107,6 +201,10 @@ body <- dashboardBody(
                                                           "total_SO2",
                                                           "quality"), 
                                               selected = "alcohol"),
+                           selectInput("row_choice", "Filter by Wine Type",
+                                       choices = c("Include All", 
+                                                   "Red Only",
+                                                   "White Only")),
                 downloadButton('downloadData', 'Download')
               ),
                 mainPanel(h1("Table of Selected Data"), dataTableOutput("data_table")
@@ -119,7 +217,7 @@ body <- dashboardBody(
 )
 
 dashboardPage(
-  dashboardHeader(title = "Wine App"),
+  dashboardHeader(title = "A Sip of Stats"),
   sidebar,
   body
 )
